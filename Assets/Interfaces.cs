ba7b3ed77fdabcfd7678697df74f1f9ef33ebc87
Emirhan Şenkal <emirhansenkal1@gmail.com>
2021-03-12 11:05:41 +0300

    Initialize Project

---
+++ Assets/Interfaces.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageable<T>
+{
+    T Health { get; set; }
+    
+    void Damage(T damageTaken);
+}
+
+public interface IKillable
+{
+    void Kill();
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour,IDamageable<float>, IKillable
+{
+    private float _health = 100;
+
+    public float Health 
+    { 
+        get => _health;
+        set => _health = value;
+    }
+
+    public void Damage(float damageTaken)
+    {
+        _health -= damageTaken;
+    }
+
+    public void Kill()
+    {
+        _health = 0;
+    }
+}
